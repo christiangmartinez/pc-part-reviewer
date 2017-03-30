cf734b3b9dc8f57137a22137d591d1afeed10600
Christian Martinez <cgmartinez11142@gmail.com>
2017-03-30 15:43:49 -0700

    add methods and tests to save to database, assign id, and find component by id

--- src/main/java/Component.java
+++ src/main/java/Component.java
@@ -8,14 +8,14 @@ public class Component {
   private String name;
   private int price;
   private String review;
-  private int manufacturerId;
+  // private int manufacturerId; (remember to add this to the constructor as well!!!)
 
-  public Component(String type, String name, int price, String review, int manufacturerId) {
+  public Component(String type, String name, int price, String review) {
     this.type = type;
     this.name = name;
     this.price = price;
     this.review = review;
-    this.manufacturerId = manufacturerId;
+    // this.manufacturerId = manufacturerId;
   }
 
   public int getId() {
@@ -38,9 +38,9 @@ public class Component {
     return review;
   }
 
-  public int getManufacturerId() {
-    return manufacturerId;
-  }
+  // public int getManufacturerId() {
+  //   return manufacturerId;
+  // }
 
   @Override
   public boolean equals(Object otherComponent) {
@@ -51,8 +51,39 @@ public class Component {
       return this.getType().equals(newComponent.getType())
         && this.getName().equals(newComponent.getName())
         && this.getPrice() == newComponent.getPrice()
-        && this.getReview().equals(newComponent.getReview())
-        && this.getManufacturerId() == newComponent.getManufacturerId();
+        && this.getReview().equals(newComponent.getReview());
+        // && this.getManufacturerId() == newComponent.getManufacturerId();
+    }
+  }
+
+  public static List<Component> all() {
+    //add manufacturerId to SELECT after it's created
+    String sql = "SELECT id, type, name, price, review FROM components";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Component.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      //add manufacturerId to INSERT INTO and VALUES after it's created
+      String sql = "INSERT INTO components(type, name, price, review) VALUES (:type, :name, :price, :review)";
+      this.id = (int) con.createQuery(sql, true)
+      // .addParameter("manufacturerId", this.manufacturerId)
+      .addParameter("type", this.type)
+      .addParameter("name", this.name)
+      .addParameter("price", this.price)
+      .addParameter("review", this.review)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Component find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM components where id = :id";
+      Component component = con.createQuery(sql).addParameter("id", id).executeAndFetchFirst(Component.class);
+      return component;
     }
   }
 
--- src/test/java/ComponentTest.java
+++ src/test/java/ComponentTest.java
@@ -9,27 +9,59 @@ public class ComponentTest {
 
   @Test
   public void component_instantiatesCorrectly_true() {
-    Component testComponent = new Component("GPU", "GTX 1070", 400, "good", 1);
+    Component testComponent = new Component("GPU", "GTX 1070", 400, "good");
     assertTrue(testComponent instanceof Component);
   }
 
   @Test
   public void getters_returnCorrectValues_variables() {
-    Component testComponent = new Component("GPU", "GTX 1070", 400, "good", 1);
+    Component testComponent = new Component("GPU", "GTX 1070", 400, "good");
     assertEquals("GPU", testComponent.getType());
     assertEquals("GTX 1070", testComponent.getName());
     assertEquals(400, testComponent.getPrice());
     assertEquals("good", testComponent.getReview());
-    assertEquals(1, testComponent.getManufacturerId());
+    // assertEquals(1, testComponent.getManufacturerId());
   }
 
   @Test
   public void equals_returnsTrueIfDescriptionsAreTheSame() {
-    Component firstComponent = new Component("GPU", "GTX 1070", 400, "good", 1);
-    Component secondComponent = new Component("GPU", "GTX 1070", 400, "good", 1);
+    Component firstComponent = new Component("GPU", "GTX 1070", 400, "good");
+    Component secondComponent = new Component("GPU", "GTX 1070", 400, "good");
     assertTrue(firstComponent.equals(secondComponent));
   }
 
+  @Test
+  public void all_returnsAllInstancesOfComponent_true() {
+    Component firstComponent = new Component("GPU", "GTX 1070", 400, "good");
+    firstComponent.save();
+    Component secondComponent = new Component("GPU", "GTX 1070", 400, "good");
+    secondComponent.save();
+    assertTrue(Component.all().get(0).equals(firstComponent));
+    assertTrue(Component.all().get(1).equals(secondComponent));
+  }
 
+  @Test
+  public void save_savesIntoDatabase_True() {
+    Component testComponent = new Component("GPU", "GTX 1070", 400, "good");
+    testComponent.save();
+    assertTrue(Component.all().get(0).equals(testComponent));
+  }
+
+  @Test
+  public void save_assignsIdToComponent() {
+    Component testComponent = new Component("GPU", "GTX 1070", 400, "good");
+    testComponent.save();
+    Component savedComponent = Component.all().get(0);
+    assertEquals(testComponent.getId(), savedComponent.getId());
+  }
+
+  @Test
+  public void find_returnsComponentsWithSameId_secondComponent() {
+    Component firstComponent = new Component("GPU", "GTX 1070", 400, "good");
+    firstComponent.save();
+    Component secondComponent = new Component("CPU", "1800x", 500, "good");
+    secondComponent.save();
+    assertEquals(secondComponent, Component.find(secondComponent.getId()));
+  }
 
 }
